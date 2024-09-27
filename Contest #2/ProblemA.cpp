2600cfe24b133eacd747fca161d127d810232ba7
GeorgeBlaize <92225843+GeorgeBlaize@users.noreply.github.com>
2024-09-27 15:40:03 +0600

    Create ProblemA.cpp

---
+++ Contest #2/ProblemA.cpp
@@ -0,0 +1,21 @@
+//Timon and Pumbaa
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+   int a,b;
+   cin>>a>>b;
+   int result=a-b;
+
+   if(result<0){
+
+      result=0;
+   }
+
+   cout<<result<<endl;
+
+   return 0;
+
+}
