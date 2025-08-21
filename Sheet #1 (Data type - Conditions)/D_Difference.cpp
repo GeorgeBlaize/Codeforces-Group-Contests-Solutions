6c184ad3882b5a1ea18e3ba43a6394fca2a22f7e
GeorgeBlaize <92225843+GeorgeBlaize@users.noreply.github.com>
2025-08-22 00:32:35 +0600

    Add files via upload

---
+++ Sheet #1 (Data type - Conditions)/D_Difference.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long int a,b,c,d;
+    cin >> a >> b >> c >> d;
+
+    long long int x;
+
+    x=(a*b)-(c*d);
+
+    cout << "Difference = " << x << endl;
+
+    return 0;
+}
